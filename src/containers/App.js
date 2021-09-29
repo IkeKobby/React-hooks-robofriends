b8557c32f3e60963e430e4ed8da67775c6b8a288
ikekobby <annqwabena@gmail.com>
2021-09-29 13:49:50 +0000

    React app for robofriends

---
+++ src/containers/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+function App() {
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     robots: [],
+  //     searchfield: ''
+  //   }
+  // }
+  const [robots, setRobots] = useState([])
+  const [searchfield, setSearchfield] = useState('')
+
+  // componentDidMount() {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then(response=> response.json())
+  //     .then(users => {this.setState({ robots: users})});
+  // }
+  useEffect(()=> {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {setRobots(users)});
+  }, [])
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value)
+  }
+
+  const filteredRobots = robots.filter(robot =>{
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  })
+
+  return !robots.length ?
+    <h1>Loading</h1> :
+    (
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange}/>
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+
+}
+
+export default App;
