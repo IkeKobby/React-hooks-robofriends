d2a8e0f770ef065e50b2f92c34698621383a52c6
ikekobby <annqwabena@gmail.com>
2021-09-29 14:36:13 +0000

    Updates

---
+++ precache-manifest.8b2287847bc711d12a16552f9d04f5c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19cdb33b95afb61f3ac16ea892cdf9f",
+    "url": "/React-hooks-robofriends/index.html"
+  },
+  {
+    "revision": "fdb1fab9470c9c8edc75",
+    "url": "/React-hooks-robofriends/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "776bd09438a6ce05f509",
+    "url": "/React-hooks-robofriends/static/css/main.57556511.chunk.css"
+  },
+  {
+    "revision": "fdb1fab9470c9c8edc75",
+    "url": "/React-hooks-robofriends/static/js/2.ac186811.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-hooks-robofriends/static/js/2.ac186811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776bd09438a6ce05f509",
+    "url": "/React-hooks-robofriends/static/js/main.b9fd5bce.chunk.js"
+  },
+  {
+    "revision": "cf2b54445e8319ec039a",
+    "url": "/React-hooks-robofriends/static/js/runtime-main.67cf45d0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React-hooks-robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
